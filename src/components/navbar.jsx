5f2c7e6d631bcb983356d537a6595c09da8d33dd
keenu1 <nugrohorizki191@gmail.com>
2024-04-12 18:36:50 +0700

    update provile responsive, navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -44,7 +44,6 @@ function Login() {
 
   const dropdownProfileToggle = () => {
     setDropdownProfile(!dropdownProfile);
-
     // console.log(dropdownProfile);
   };
   const showMenuToggle = () => {
@@ -77,14 +76,15 @@ function Login() {
     const parent = event.target.parentNode;
 
     if (event.target.tagName === "HTML"
-
     ) {
       setDropdownProfile(false);
       // console.log("mousedown event occurred on a button");
     } else if (event.target.id != undefined && (event.target.tagName === "BUTTON" || parent.parentNode.id == "dropdownmenuprofile" ||
       event.target.id == 'menu-icon' ||
-      event.target.id == 'profil-name')) {
-
+      event.target.id == 'profil-name' ||
+      event.target.id == 'menu-profile')
+    ) {
+      // setDropdownProfile(!dropdownProfile);
     } else {
       setDropdownProfile(false);
     }
@@ -112,26 +112,25 @@ function Login() {
 
   return (
     <>
-      <aside className={`bg-white h-screen shadow-lg rounded-r-xl absolute top-0  transition-all duration-500 ease-in-out ${showMenu ? "w-1/5  " : "w-16"}`}>
-
+      <aside className={`hidden md:block bg-white h-screen shadow-lg rounded-r-xl absolute top-0  transition-all duration-500 ease-in-out ${showMenu ? "w-1/5  " : "w-16"}`}>
         <nav className="flex flex-col justify-between  items-center h-full">
           <div className={`flex flex-col  gap-4 p-4 w-full ${showMenu ? "items-start" : "items-center"}`}>
             <div className="flex justify-between  items-center w-full">
               <div className={`flex items-center   w-full font-semibold`}>
-                <div className="flex">
+                <div className="flex items-center gap-2 ">
                   <img
                     src="/src/assets/img/logo_tab.jpeg"
                     alt=""
                     className={`rounded-full w-7 flex-none ${showMenu ? 'w-7' : 'w-7'}`}
 
                   />
-                  <span className={`ms-2 transition-all  flex-none ${showMenu ? ' delay-300 duration-200 ease-in-out opacity-100 translate-x-0 ' : 'opacity-0 duration-0 translate-x-[-10px]'}`}>
+                  <span className={`ms-2 transition-all md:text-transparent md:text-xs lg:text-lg  flex-none  lg:text-black ${showMenu ? ' delay-300 duration-200 ease-in-out opacity-100 translate-x-0 ' : 'opacity-0 duration-0 translate-x-[-10px]'}`}>
                     Gamify Life
                   </span>
                 </div>
 
               </div>
-              <div className={`bg-gray-100 rounded-lg  px-1 hover:cursor-pointer hover:bg-gray-200 ${showMenu ? "" : "hidden"}`}>
+              <div className={`bg-gray-100 rounded-lg z-10  px-1 hover:cursor-pointer hover:bg-gray-200 ${showMenu ? "" : "hidden"}`}>
                 <i className='bx bx-chevron-left mt-1' onClick={showMenuToggle}></i>
               </div>
             </div>
@@ -159,7 +158,7 @@ function Login() {
               </Link>
               <Link
                 to="/news"
-                className={`${isActive("/dashboard") ? "bg-gray-100 text-black font-medium" : "text-gray-500 hover:font-medium hover:text-black"
+                className={`${isActive("/News") ? "bg-gray-100 text-black font-medium" : "text-gray-500 hover:font-medium hover:text-black"
                   } flex items-center rounded-lg px-3 py-2`}
               >
                 <i className='bx bxs-news' ></i>
@@ -169,7 +168,7 @@ function Login() {
             </div>
 
           </div>
-          <div className="p-3 w-full  ">
+          <div className="p-3 w-full relative ">
             <div className="w-full  flex justify-center items-center gap-2">
               <div className="w-8 h-8 rounded-lg  " style={Profile}>
 
@@ -197,7 +196,7 @@ function Login() {
             </div>
 
             <div
-              className={`absolute bottom-0 mb-3 ms-52  z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none  ${dropdownProfile
+              className={`absolute bottom-0 mb-3 -me-2   z-10 mt-2 w-full -right-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none  ${dropdownProfile
                 ? "transform opacity-100 scale-100 transition ease-out duration-100 "
                 : "transform opacity-0 scale-95 transition ease-in duration-75  pointer-events-none"
                 }`}
@@ -237,6 +236,135 @@ function Login() {
         </nav>
 
       </aside>
+
+      <aside className={`border block md:hidden bg-white w-screen shadow-lg rounded-t-xl absolute bottom-0  transition-all duration-500 ease-in-out ${showMenu ? "w-1/5  " : "w-16"}`}>
+        <nav className="flex relative flex-row justify-between  items-center h-full">
+          <div className={`flex flex-row items-start p-4  `}>
+            <div className="flex">
+              <img
+                src="/src/assets/img/logo_tab.jpeg"
+                alt=""
+                className={`rounded-full w-7 flex-none ${showMenu ? 'w-7' : 'w-7'}`}
+
+              />
+              <span className={`ms-2 transition-all  flex-none ${showMenu ? ' delay-300 duration-200 ease-in-out opacity-100 translate-x-0 ' : 'opacity-0 duration-0 translate-x-[-10px]'}`}>
+                {/* Gamify Life */}
+              </span>
+            </div>
+            {/* <div className={`bg-gray-100 rounded-lg  px-1 hover:cursor-pointer hover:bg-gray-200 ${showMenu ? "" : "hidden"}`}>
+                <i className='bx bx-chevron-left mt-1' onClick={showMenuToggle}></i>
+              </div> */}
+
+            {/* <div className={`bg-gray-100 rounded-lg  px-1 hover:cursor-pointer hover:bg-gray-200 ${showMenu ? "hidden" : ""}`}>
+              <i className='bx bx-chevron-right mt-1' onClick={showMenuToggle}></i>
+            </div> */}
+
+
+
+          </div >
+          <div className={`flex flex-row gap-2 ${showMenu ? "" : ""}`}>
+            <Link
+              to="/home"
+              className={`${isActive("/home") ? "bg-gray-100 text-black font-medium " : "text-gray-500 hover:font-medium hover:text-black "
+                } relative flex items-center rounded-lg px-3 py-2`}
+            >
+              <i className='bx bx-home-circle '></i>
+
+              <span className={`ms-6 flex-none absolute   transition-all  ${showMenu ? "opacity-100 transform translate-x-0 duration-200 delay-300" : "opacity-0 translate-x-[-20px] duration-0 "}`}>
+                {/* Home */}
+              </span>
+            </Link>
+            <Link
+              to="/dashboard"
+              className={`${isActive("/dashboard") ? "bg-gray-100 text-black font-medium" : "text-gray-500 hover:font-medium hover:text-black"
+                } flex items-center rounded-lg px-3 py-2`}
+            >
+              <i className='bx bxs-dashboard' ></i>
+              <span className={`ms-6 flex-none absolute   transition-all  ${showMenu ? "opacity-100 transform translate-x-0 duration-200 delay-500" : "opacity-0 translate-x-[-20px] duration-0 "}`}>
+                {/* Dashboard */}
+              </span>
+            </Link>
+            <Link
+              to="/news"
+              className={`${isActive("/news") ? "bg-gray-100 text-black font-medium" : "text-gray-500 hover:font-medium hover:text-black"
+                } flex items-center rounded-lg px-3 py-2`}
+            >
+              <i className='bx bxs-news' ></i>
+              <span className={`ms-6 flex-none absolute   transition-all  ${showMenu ? "opacity-100 transform translate-x-0 duration-200 delay-700" : "opacity-0 translate-x-[-20px] duration-0 "}`}>
+                {/* News */}
+              </span>
+
+            </Link>
+          </div>
+          <div className="p-3 ">
+            <div className=" flex  justify-end items-center gap-2">
+              <div className="w-8 h-8 rounded-lg " style={Profile} id="menu-profile" onClick={dropdownProfileToggle}>
+
+              </div>
+              {/* <div className={`grow ${showMenu ? "" : "hidden"}`}>
+                <button
+                  type="button"
+                  className={`inline-flex w-full justify-between items-center gap-x-1.5   px-3 py-2 text-sm font-semibold text-gray-900 rounded-lg hover:bg-gray-100 ${dropdownProfile ? "bg-gray-100" : "bg-white"} `}
+                  id="menu-button"
+                  aria-expanded={dropdownProfile}
+                  aria-haspopup="true"
+                  onClick={dropdownProfileToggle}
+                >
+                  <span className={`ms-2 `} id="profil-name">
+                    {nama}
+
+                  </span>
+                  <i id="menu-icon" className={`bx bx-dots-vertical-rounded z-0 `}></i>
+
+
+                </button>
+              </div> */}
+
+            </div>
+
+
+          </div>
+
+
+          <div
+            className={`absolute right-0 bottom-14 mb-3 ms-52  z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none  ${dropdownProfile
+              ? "transform opacity-100 scale-100 transition ease-out duration-100 "
+              : "transform opacity-0 scale-95 transition ease-in duration-75  pointer-events-none"
+              }`}
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="menu-button"
+            tabIndex="-1"
+            id="dropdownmenuprofile"
+          >
+            <div className="py-1" role="none">
+              <Link
+                to="/profile"
+                className="text-gray-700 flex items-center px-4 py-2 text-sm hover:font-medium hover:text-black"
+                role="menuitem"
+                tabIndex="-1"
+                id="menu-item-0"
+              >
+                <i className='bx bx-user me-2'></i> Profil
+              </Link>
+
+              <form onSubmit={logOut}>
+                <button
+                  type="submit"
+                  className="text-gray-700 w-full px-4 py-2 text-left text-sm flex items-center  hover:text-red-500 hover:font-medium"
+                  role="menuitem"
+                  tabIndex="-1"
+                  id="menu-item-3"
+                >
+                  <i className='bx bx-log-out me-2'></i>Log out
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav >
+
+
+      </aside >
 
     </>
   );
--- src/pages/profil/index.jsx
+++ src/pages/profil/index.jsx
@@ -10,17 +10,23 @@ import {
   alertPopupError,
   alertBottom
 } from "../../assets/js/function";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Fragment, useRef } from "react";
+import { Dialog, Transition } from '@headlessui/react'
 import Cookies from "js-cookie";
 import axios from "axios";
 // import iziToast from "izitoast";
 
 function Profil() {
   const [showMenu, setShowMenu] = useState(true);
+  const [open, setOpen] = useState(false)
+
+  const cancelButtonRef = useRef(null)
+
   const [currentData, setcurrentData] = useState({
     name: "",
     gender: "",
     dob: "",
+    image: "",
   });
   const goPage = useNavigation();
 
@@ -47,6 +53,7 @@ function Profil() {
     axios
       .post(api, "", config)
       .then((response) => {
+        console.log(response.data.data);
         if (response.data.status === true) {
           const newData = { ...response.data.data[0] }; // Create a new object
           if (newData.dob === "0000-00-00") {
@@ -63,6 +70,8 @@ function Profil() {
         alert("Mohon maaf terjadi kesalahan silahkan coba lagi ");
         console.log(error);
       });
+
+
   };
 
   const updateData = (event) => {
@@ -73,6 +82,14 @@ function Profil() {
     Object.entries(currentData).forEach(([key, value]) => {
       formData.append(key, value);
     });
+
+    var input = document.getElementById('file_input');
+
+    // Check if a file is selected
+    if (input.files && input.files[0]) {
+      var file = input.files[0]; // Get the file object
+      formData.append('image', file); // A
+    }
 
     const config = {
       headers: { Authorization: `Bearer ` + Cookies.get("token") },
@@ -90,6 +107,7 @@ function Profil() {
           }
           setcurrentData(response.data.data[0]);
           alertBottom('Updated', response.data.message);
+          console.log(response.data);
           CloseLoading();
 
           // alert(response.data.message);
@@ -113,23 +131,39 @@ function Profil() {
     loadData();
   }, []);
 
+  const Profile = {
+    backgroundImage: 'url("' + currentData.image + '")',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    // Adjust as needed
+  };
+
   return (
     <>
 
       <Navbar />
-      <div className="flex">
-        <div className={` transition-all ease-in-out duration-500 ${showMenu ? "w-1/5" : "w-16"}`} ></div>
-        <div className={` grow mx-5 mt-5 border p-5 rounded-xl shadow-lg ${showMenu ? "" : ""}`}>
+      <div className={`flex transition-all ease-in-out duration-500    ${showMenu ? "md:gap-11 lg:gap-2" : "md:gap-4 lg:gap-2"}`}>
+        <div className={`  transition-all ease-in-out duration-500 ${showMenu ? "w-0 md:w-1/5" : "w-0 md:w-16"}`} ></div>
+        <div className={` transition-all ease-in-out duration-500 grow  mt-5 border p-5 rounded-xl shadow-lg mx-2 ${showMenu ? " " : ""}`}>
           <form onSubmit={updateData}>
             <div className="grid gap-6 mb-6 md:grid-cols-4 ">
-              <div className="flex justify-center">
-                <div className="overflow-hidden rounded-full w-32 h-32  ">
-                  <img
+              <div className="flex justify-center ">
+                <div className="  w-32 h-32  relative" >
+                  <div className=" object-cover rounded-full w-full h-full " style={Profile}>
+
+                  </div>
+
+                  {/* <img
                     src={defaultImage}
                     alt=""
                     className="object-cover w-full h-full"
-                  />
+                  /> */}
+                  <div className="absolute z-10 bg-black right-0 p-2 rounded-full bottom-0   text-white flex justify-start hover:cursor-pointer" onClick={() => setOpen(true)}>
+                    <i className='bx bxs-pencil'></i>
+                  </div>
                 </div>
+
               </div>
               <div>
                 <label
@@ -153,7 +187,7 @@ function Profil() {
                   htmlFor="gender"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Select an option
+                  Select Gender
                 </label>
                 <select
                   id="gender"
@@ -187,15 +221,92 @@ function Profil() {
               <div>
                 <button
                   type="submit"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="text-white bg-black hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
                 >
                   Submit
                 </button>
               </div>
             </div>
+
+
+
+
+
           </form>
         </div>
       </div>
+
+
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <form onSubmit={updateData}>
+                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                      <div className="sm:flex sm:items-start">
+                        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left w-full">
+                          <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                            Update your Image
+                          </Dialog.Title>
+                          <div className="mt-2  ">
+                            {/* <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label> */}
+                            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file" />
+                            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
+
+
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                      <button
+                        type="submit"
+                        className="ms-2 text-white bg-black hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                        onClick={() => setOpen(false)}
+                      >
+                        Save
+                      </button>
+                      <button
+                        type="button"
+                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                        onClick={() => setOpen(false)}
+                        ref={cancelButtonRef}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </form>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+
+
+
 
     </>
   );
